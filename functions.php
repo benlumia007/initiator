592081cebed1efaf89af2e74d57e784874c72497
Benjamin Lu <benlumia007@gmail.com>
2019-04-17 19:53:41 -0700

    autoload backdrop core

--- functions.php
+++ functions.php
@@ -65,6 +65,9 @@ function initiator_upgrade_notice() {
 /**
  * 2.0 - Backdrop Core
  */
+if ( file_exists( get_parent_theme_file_path( '/vendor/autoload.php' ) ) ) {
+	require_once get_parent_theme_file_path( '/vendor/autoload.php' );
+}
 
 /**
  * 3.0 - Framework
