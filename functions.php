56fe044273c88d21ffab4708ab33507497fec7ba
Benjamin Lu <benlumia007@gmail.com>
2019-04-17 19:47:57 -0700

    initial commit

---
+++ functions.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Initiator ( functions.php )
+ *
+ * This (functions.php) template file should only do three jobs, one is to check the compatibility if the site meets
+ * the miminum requirements before the theme proceeds to activate. The second job is to autoload the backdrop core
+ * famework. The last job is to require the theme's framework so that it can add additional features if the backdrop
+ * core doesn't come with features the theme needs.
+ *
+ * @package     Initiator
+ * @copyright   Copyright (C) 2018-2019. Benjamin Lu
+ * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author      Benjamin Lu ( https://benjlu.com )
+ */
+
+/**
+ * Table of Content
+ *
+ * 1.0 - Compatibility Check
+ * 2.0 - Backdrop Core
+ * 3.0 - Framework
+ */
+
+/**
+ * 1.0 - Compatibility Check
+ */
+
+/**
+ * 2.0 - Backdrop Core
+ */
+
+/**
+ * 3.0 - Framework
+ */
