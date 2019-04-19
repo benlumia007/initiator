80cc339c679fa098b446a54a2b1b85357ffe0801
Benjamin Lu <benlumia007@gmail.com>
2019-04-19 00:25:38 -0700

    added customizer

--- framework/framework.php
+++ framework/framework.php
@@ -17,8 +17,13 @@
 /**
  * 1.0 - Create a new Framework
  */
-$initiator = new Benlumia007\Backdrop\Framework();
-$sidebar   = new Benlumia007\Backdrop\Framework\Sidebar();
-$menus     = new Benlumia007\Backdrop\Framework\Menu();
+$initiator  = new Benlumia007\Backdrop\Framework();
+$sidebar    = new Benlumia007\Backdrop\Framework\Sidebar();
+$menus      = new Benlumia007\Backdrop\Framework\Menu();
+$customizer = new Benlumia007\Backdrop\Framework\Customizer();
 
-
+function test() {
+	add_theme_support('custom-background');
+	add_theme_support( 'custom-header' );
+}	
+add_action( 'after_setup_theme', 'test' );
