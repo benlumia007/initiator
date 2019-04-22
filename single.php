60acc2c43c6ee56c707bf4f3eab82e4f84494e9d
Benjamin Lu <benlumia007@gmail.com>
2019-04-22 15:31:19 -0700

    initial commit

---
+++ single.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Initiator ( single.php )
+ *
+ * @package     Initiator
+ * @copyright   Copyright (C) 2018-2019. Benjamin Lu
+ * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author      Benjamin Lu ( https://benjlu.com )
+ */
+
+?>
+<?php get_header(); ?>
+	<section id="content" class="site-content">
+		<div id="global-layout" class="<?php echo esc_attr( get_theme_mod( 'global_layout', 'no-sidebar' ) ); ?>">
+			<main id="main" class="content-area">
+				<?php Benlumia007\Backdrop\MainQuery\display( 'content-single' ); ?>
+			</main>
+			<?php Benlumia007\Backdrop\Sidebar\display( 'primary' ); ?>
+		</div>
+	</div>
+<?php get_footer(); ?>
