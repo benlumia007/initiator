5a836b227ccc1fb03b1cad9c1eb629442bebed64
Benjamin Lu <benlumia007@gmail.com>
2019-04-22 08:43:13 -0700

    initial commit

---
+++ header.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Initiator ( index.php )
+ *
+ * The (index.php) template file is very flexible, it can be used to include all the references for the header, content,
+ * aside, footer, and other pages created in WordPress. The (index.php) can also be divided into modular template files,
+ * each taking on part of the workload. If you wish to not provide any other template files, the WordPress hierarchy may
+ * have default template files or functions to perform their jobs.
+ *
+ * @package     Initiator
+ * @copyright   Copyright (C) 2019. Benjamin Lu
+ * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
+ * @author      Benjamin Lu ( https://benjlu.com )
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<div id="container" class="site-container">
+	<header id="header" class="site-header">
+		<div class="site-branding">
+			<?php Benlumia007\Backdrop\Site\display( 'site-title' ); ?>
+			<?php Benlumia007\Backdrop\Site\display( 'site-description' ); ?>
+		</div>
+	</header>
