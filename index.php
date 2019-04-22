1d44061085116edc128d1b71f60021be020d9baa
Benjamin Lu <benlumia007@gmail.com>
2019-04-22 08:44:56 -0700

    moved header and footer to its own template

--- index.php
+++ index.php
@@ -14,44 +14,6 @@
  */
 
 ?>
-<!doctype html>
-<html <?php language_attributes(); ?>>
-<head>
-<?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
-<div id="container" class="site-container">
-	<header id="header" class="site-header">
-		<div class="site-branding">
-			<?php Benlumia007\Backdrop\Site\display( 'site-title' ); ?>
-			<?php Benlumia007\Backdrop\Site\display( 'site-description' ); ?>
-		</div>
-	</header>
-	<section id="content" class="site-content">
+<?php get_header(); ?>
 
-	</section>
-	<footer id="footer" class="site-footer">
-		<div class="site-info">
-			<?php
-			printf(
-				// Translators: 1 = Date, 2 = Site Link.
-				esc_html__( 'Copyright &#169; %1$s. %2$s', 'initiator' ),
-				absint( date_i18n( 'Y' ) ),
-				Benlumia007\Backdrop\Site\display( 'site-link' ) // phpcs:ignore
-			);
-			?>
-			<br />
-			<?php
-			printf(
-				// Translators: 1 = WordPress Link, 2 = Theme Link.
-				esc_html__( 'Powered By %1$s and %2$s', 'initiator' ),
-				Benlumia007\Backdrop\Site\display( 'wp-link' ), // phpcs:ignore
-				Benlumia007\Backdrop\Site\display( 'theme-link' ) // phpcs:ignore
-			);
-			?>
-		</div>
-	</footer>
-</div>
-<?php wp_footer(); ?>
-</body>
-</html>
+<?php get_footer(); ?>
