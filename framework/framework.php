397953c72c8646b70e1b4378cbe40cb981f774ce
Benjamin Lu <benlumia007@gmail.com>
2019-04-22 22:01:26 -0700

    update with doc informations

--- framework/framework.php
+++ framework/framework.php
@@ -2,6 +2,8 @@
 /**
  * Initiator ( framework.php )
  *
+ * This file is used to create a new framework instance and adds specific features to the theme.
+ *
  * @package     Initiator
  * @copyright   Copyright (C) 2019. Benjamin Lu
  * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
@@ -9,17 +11,15 @@
  */
 
 /**
- * Table of Content
+ * Create a new framework instance
  *
- * 1.0 - Create a New Framework
- */
-
-/**
- * 1.0 - Create a New Framework
- *
- * This will initialize te Backdrop Core Framework and will add all the necessary components and features
- * to the theme, such as Menu, Sidebar, and Global Layout.
+ * This will create an instance of the framework allowing you to initialize within the theme.
  */
 $framework = Benlumia007\Backdrop\Framework::get_instance();
 
+/**
+ * Loads boot();
+ *
+ * Creates and calls and boots additional features, allowing the theme to fully function.
+ */
 $framework->boot();
