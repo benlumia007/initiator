f80e94d5806e0729b2b1d9413874b1df43936dda
Benjamin Lu <benlumia007@gmail.com>
2019-04-22 22:55:41 -0700

    added other theme support

--- framework/functions-setup.php
+++ framework/functions-setup.php
@@ -36,6 +36,32 @@ add_action(
 		$GLOBALS['content_width'] = 810;
 
 		/**
+		 * By adding add_theme_support( 'title-tag' );, this will let WordPress manage all document titles and should be use instead of wp_title();.
+		 */
+		add_theme_support( 'title-tag' );
+
+		/**
+		 * By adding add_theme_support( 'automatic-feed-links' );, this feature when enabled allows feed links for post and comments
+		 * in the head of the theme. This feature should be used in place of of the deprecated function automatic_feed_links();.
+		 */
+		add_theme_support( 'automatic-feed-links' );
+
+		/**
+		 * By adding add_theme_support( 'html5', arrayy() );, this feature when enabled allows the user use of HTML5 markup for
+		 * comment list, comment forms, search forms, galleries, and captions.
+		 */
+		add_theme_support(
+			'html5',
+			array(
+				'comment-list',
+				'comment-form',
+				'search-form',
+				'gallery',
+				'caption',
+			)
+		);
+
+		/**
 		 * By adding add_theme_support( 'post-thumbnails' );, this feature when enabled allows you to setup featured images
 		 * also known as featured image. If you need to use conditional, please consider using has_post_thumbnail.
 		 */
