f5eecfa7b71233d492dfedd3138d23047eb5cf99
Benjamin Lu <benlumia007@gmail.com>
2019-04-24 01:25:31 -0700

    used the new entry::

--- views/content/content.php
+++ views/content/content.php
@@ -7,14 +7,16 @@
  * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
  * @author      Benjamin Lu ( https://benjlu.com )
  */
+
+use Benlumia007\Backdrop\Entry\Entry as entry;
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php if ( is_sticky() && is_home() && ! is_paged() ) { ?>
 			<?php printf( '<span class="sticky-post">%1$s</span>', esc_html__( 'Featured', 'initiator' ) ); ?>
 		<?php } ?>
-		<?php Benlumia007\Backdrop\Entry\display( 'entry-title' ); ?>
-		<span class="entry-timestamp"><?php Benlumia007\Backdrop\Entry\display( 'entry-posted-on' ); ?></span>
+		<?php entry::display( 'entry-title' ); ?>
+		<span class="entry-metadata"><?php entry::display( 'posted-on' ); ?></span>
 	</header>
 	<div class="entry-excerpt">
 		<?php the_excerpt(); ?>
