122ddb4655e56a9ddb45ad99701a2f5072e59fb3
Benjamin Lu <benlumia007@gmail.com>
2019-04-25 08:01:01 -0700

    moved loop back to theme.

--- archive.php
+++ archive.php
@@ -8,14 +8,26 @@
  * @author      Benjamin Lu ( https://benjlu.com )
  */
 
-use Benlumia007\Backdrop\View\View as loop;
 use Benlumia007\Backdrop\View\View as sidebar;
 ?>
 <?php get_header(); ?>
 	<section id="content" class="site-content">
 		<div id="global-layout" class="<?php echo esc_attr( get_theme_mod( 'global_layout', 'no-sidebar' ) ); ?>">
 			<main id="main" class="content-area">
-				<?php loop::display( 'loop', [ 'content-archive' ] ); ?>
+				<?php
+					if ( have_posts() ) : ?>
+						<header class="archive-header">
+							<h1 class="archive-title"><?php the_archive_title(); ?></h1>
+						</header>
+				<?php
+					while ( have_posts() ) : the_post();
+						get_template_part( 'views/content/content', get_post_format() );
+					endwhile;
+						the_posts_pagination();
+					else :
+						get_template_part( 'views/content/content', 'none' );
+					endif;
+				?>
 			</main>
 			<?php sidebar::display( 'sidebar', [ 'primary' ] ); ?>
 		</div>
