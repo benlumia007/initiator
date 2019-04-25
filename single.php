c8da115d4f559892bb0ca6f5ed9fac138b70dde5
Benjamin Lu <benlumia007@gmail.com>
2019-04-25 07:53:29 -0700

    moved loop back to theme.

--- single.php
+++ single.php
@@ -8,14 +8,24 @@
  * @author      Benjamin Lu ( https://benjlu.com )
  */
 
-use Benlumia007\Backdrop\View\View as loop;
 use Benlumia007\Backdrop\View\View as sidebar;
 ?>
 <?php get_header(); ?>
 	<section id="content" class="site-content">
 		<div id="global-layout" class="<?php echo esc_attr( get_theme_mod( 'global_layout', 'no-sidebar' ) ); ?>">
 			<main id="main" class="content-area">
-				<?php loop::display( 'loop', [ 'content-single' ] ); ?>
+				<?php
+					while ( have_posts() ) : the_post();
+						get_template_part( 'views/content/content', 'single' );
+					endwhile;
+						the_post_navigation(
+							array(
+								'next_text' => '<span class="post-next" aria-hiddent="true">' . esc_html__( 'Next', 'initiator' ) . '</span><span class="post-title">%title</span>',
+								'prev_text' => '<span class="post-previous" aria-hidden="true">' . esc_html__( 'Previous', 'initiator' ) . '</span><span class="post-title">%title</span>',
+							)
+						);
+				comments_template();
+				?>
 			</main>
 			<?php sidebar::display( 'sidebar', [ 'primary' ] ); ?>
 		</div>
