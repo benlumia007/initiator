844311b57db6b38f73bd1c76535723ee756182a2
Benjamin Lu <benlumia007@gmail.com>
2019-04-29 10:59:02 -0700

    changed copyright

--- page.php
+++ page.php
@@ -3,7 +3,7 @@
  * Initiator ( page.php )
  *
  * @package     Initiator
- * @copyright   Copyright (C) 2018-2019. Benjamin Lu
+ * @copyright   Copyright (C) 2019. Benjamin Lu
  * @license     GNU General Public License v2 or later ( https://www.gnu.org/licenses/gpl-2.0.html )
  * @author      Benjamin Lu ( https://benjlu.com )
  */
