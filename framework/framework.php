3942f3b404c422e2321a84e85babd93f78c9db81
Benjamin Lu <benlumia007@gmail.com>
2019-04-30 00:24:53 -0700

    added default theme page

--- framework/framework.php
+++ framework/framework.php
@@ -26,4 +26,4 @@ $framework->boot();
 
 $customize = new Initiator\Component\Customize();
 
-$admin_page = new Benlumia007\Backdrop\Admin\Admin();
+$admin_page = new Benlumia007\Backdrop\Register\Admin();
