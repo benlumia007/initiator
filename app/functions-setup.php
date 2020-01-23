4453defa2b780409e006cdaae682bea4d82bbff6
Benjamin Lu <benlumia007@gmail.com>
2020-01-22 16:22:30 -0800

    add support for editor styles for blocks

--- app/functions-setup.php
+++ app/functions-setup.php
@@ -85,6 +85,9 @@ add_action( 'after_setup_theme', function() {
 		 */
 		add_theme_support( 'wp-block-styles' );
 
+		add_theme_support( 'editor-styles' );
+		add_editor_style( 'assets/css/editor-styles.css' );
+
 		/**
 		 * Load theme translation.
 		 */
