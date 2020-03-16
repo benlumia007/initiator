470e27f1eeea1cfd3a08347cc042af5df96de682
Benjamin Lu <benlumia007@gmail.com>
2020-03-16 10:23:40 -0700

    This reverts commit 5895570a218748c7b13495bad5a773a44764f293.

--- app/functions-scripts.php
+++ app/functions-scripts.php
@@ -23,7 +23,9 @@ namespace Initiator;
  * @link   https://developer.wordpress.org/reference/functions/wp_enqueue_style/
  * @link   https://developer.wordpress.org/reference/functions/wp_enqueue_script/
  */
-function initiator_enqueue_scripts_setup() {
+add_action(
+	'wp_enqueue_scripts',
+	function() {
 		/**
 		 * Rather than enqueue the main stylesheet, we are going to enqueue sceen.css since all of the styles will
 		 * go here. We only need parse the information for the Theme in style.css so that it can be activated.
@@ -41,27 +43,30 @@ function initiator_enqueue_scripts_setup() {
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
 		}
-}
-add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\initiator_enqueue_scripts_setup'); 
-
-function initiator_custom_header_inline_styles() {
-	$text_color = get_header_textcolor();
-	if ( get_theme_support( 'custom-header', 'default-text-color' ) === $text_color ) {
-		return;
 	}
-	$value      = display_header_text() ? sprintf( 'color: #%s', esc_html( $text_color ) ) : 'display: none;';
-	$custom_css = "
-		.site-branding .site-title a,
-		.site-description {
-			{$value}
+);
+
+add_action(
+	'wp_enqueue_scripts',
+	function() {
+		$text_color = get_header_textcolor();
+		if ( get_theme_support( 'custom-header', 'default-text-color' ) === $text_color ) {
+			return;
 		}
-	";
-	wp_add_inline_style( 'initiator-screen', $custom_css );
-}
-add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\initiator_custom_header_inline_styles' );
+		$value      = display_header_text() ? sprintf( 'color: #%s', esc_html( $text_color ) ) : 'display: none;';
+		$custom_css = "
+			.site-branding .site-title a,
+			.site-description {
+				{$value}
+			}
+		";
+		wp_add_inline_style( 'initiator-style', $custom_css );
+	}
+);
 
-
-function initiator_block_editor_assets() {
-	wp_enqueue_style( 'initiator-custom-fonts', get_theme_file_uri( '/vendor/benlumia007/backdrop-core/assets/fonts/custom-fonts.css' ), array(), '1.0.0' );
-}
-add_action( 'block_editor_assets', __NAMESPACE__ . '\initiator_block_editor_assets');
+add_action(
+	'enqueue_block_editor_assets',
+	function() {
+		wp_enqueue_style( 'initiator-custom-fonts', get_theme_file_uri( '/vendor/benlumia007/backdrop-core/assets/fonts/custom-fonts.css' ), array(), '1.0.0' );
+	}
+);
